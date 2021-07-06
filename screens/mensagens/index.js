38f0bbad522754a42ab6c8b1eefd29f46abe9880
Danilo Silva de Oliveira <danilo.silva.oliveira0021@gmail.com>
2021-07-06 10:24:47 -0300

    Commit de entrega

--- screens/mensagens/index.js
+++ screens/mensagens/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { ScrollView, ImageBackground,Image,TextInput } from 'react-native'
+import { ScrollView, ImageBackground,Image,TextInput, View, Text } from 'react-native'
 import { style } from './style'
 
 export function Mensagens() {
@@ -10,25 +10,103 @@ export function Mensagens() {
             <ImageBackground source={require('../../assets/icons/backgound.png')} style={style.image} >
                 <ScrollView style={style.mensagens} >
 
-                    <Image source={require('../../assets/icons/Chat1.png')} style={style.envio1} />
-                    <Image source={require('../../assets/icons/Chat2.png')} style={style.recebido} />
-                    <Image source={require('../../assets/icons/Chat3.png')} style={style.envio} />
-                    <Image source={require('../../assets/icons/Chat4.png')} style={style.recebido} />
-                    <Image source={require('../../assets/icons/Chat5.png')} style={style.envio} />
-                    <Image source={require('../../assets/icons/Chat6.png')} style={style.recebido} />
-                    <Image source={require('../../assets/icons/Chat7.png')} style={style.envio}  />
-                    <Image source={require('../../assets/icons/Chat1.png')} style={style.envio} />
-                    <Image source={require('../../assets/icons/Chat6.png')} style={style.recebido} />
-                    <Image source={require('../../assets/icons/Chat4.png')} style={style.recebido} />
-                    <Image source={require('../../assets/icons/Chat3.png')} style={style.envio} />
+                    <View>
+                        <Image source={require('../../assets/icons/Chat1.png')} style={style.envio1} />
+                        <Text style={style.mensagem1}>Mussum Ipsum, cacilds vidis litro abertis. Praesent vel viverra nisi. Mauris aliquet nunc non turpis scelerisque, eget. Pellentesque eu sodales elit.</Text>
+                        <Text style={style.data1}>22:19</Text>
+                    </View>
+
+                    <View>
+                        <Image source={require('../../assets/icons/Chat2.png')} style={style.recebido} />
+                        <Text style={style.mensagem2}>Mussum Ipsum, cacilds.</Text>
+                        <Text style={style.data2}>22:21</Text>
+                    </View>
+
+                    <View>
+                        <Image source={require('../../assets/icons/Chat3.png')} style={style.envio} />
+                        <Text style={style.mensagem3}>Lorem Ipsum is.</Text>
+                        <Text style={style.data3}>22:22</Text>
+                    </View>
+
+                    <View>
+                        <Image source={require('../../assets/icons/Chat4.png')} style={style.recebido} />
+                        <Text style={style.mensagem4}>Lorem ipsum dolor.</Text>
+                        <Text style={style.data4}>23:34</Text>
+                    </View>
+
+                    <View>
+                        <Image source={require('../../assets/icons/Chat5.png')} style={style.envio} />
+                        <Text style={style.mensagem5}>Lorem ipsum.</Text>
+                        <Text style={style.data5}>23:35</Text>
+                    </View>
+
+                    <View>
+                        <Image source={require('../../assets/icons/Chat6.png')} style={style.recebido} />
+                        <Text style={style.mensagem6}>Lorem ipsum dolor sit amet.</Text>
+                        <Text style={style.data6}>23:35</Text>
+                    </View>
+
+                    <View>
+                        <Image source={require('../../assets/icons/Chat7.png')} style={style.envio}  />
+                        <Text style={style.mensagem7}>Lorem ipsum dolor sit amet.</Text>
+                        <Text style={style.data7}>00:01</Text>
+                    </View>
+
+                    <View>
+                        <Image source={require('../../assets/icons/Chat1.png')} style={style.envio} />
+                        <Text style={style.mensagem8}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur orci ex, eleifend vitae est non, aliquam molestie erat. Donec aliquet fringilla lacinia.</Text>
+                        <Text style={style.data8}>00:01</Text>
+                    </View>
+
+                    <View>
+                        <Image source={require('../../assets/icons/Chat6.png')} style={style.recebido} />
+                        <Text style={style.mensagem6}>Lorem ipsum dolor sit amet.</Text>
+                        <Text style={style.data6}>00:21</Text>
+                    </View>
+
+                    <View>
+                        <Image source={require('../../assets/icons/Chat4.png')} style={style.recebido} />
+                        <Text style={style.mensagem4}>Lorem ipsum dolor.</Text>
+                        <Text style={style.data4}>00:21</Text>
+                    </View>
+
+                    <View>
+                        <Image source={require('../../assets/icons/Chat3.png')} style={style.envio} />
+                        <Text style={style.mensagem3}>Lorem ipsum.</Text>
+                        <Text style={style.data3}>00:22</Text>
+                    </View>
+
+                    <View>
+                        <Image source={require('../../assets/icons/Chat4.png')} style={style.recebido} />
+                        <Text style={style.mensagem4}>Lorem ipsum dolor.</Text>
+                        <Text style={style.data4}>00:31</Text>
+                    </View>
+
                     
+                    <View>
+                        <Image source={require('../../assets/icons/Chat6.png')} style={style.recebido} />
+                        <Text style={style.mensagem6}>Lorem ipsum dolor sit amet.</Text>
+                        <Text style={style.data6}>00:35</Text>
+                    </View>
+
+                    <View>
+                        <Image source={require('../../assets/icons/Chat1.png')} style={style.envio1} />
+                        <Text style={style.mensagem1}>Mussum Ipsum, cacilds vidis litro abertis. Praesent vel viverra nisi. Mauris aliquet nunc non turpis scelerisque, eget. Pellentesque eu sodales elit.</Text>
+                        <Text style={style.data1}>00:40</Text>
+                    </View>
+                    
+                    <View>
+                        <Image source={require('../../assets/icons/Chat6.png')} style={style.recebido} />
+                        <Text style={style.mensagem6}>Lorem ipsum dolor sit amet.</Text>
+                        <Text style={style.data6}>00:41</Text>
+                    </View>
                 </ScrollView>
             </ImageBackground>
 
-            <Image source={require('../../assets/icons/Emoji.png')} style={style.emoji} />
-            <TextInput onChangeText={onChangeText} value={text} style={style.caixaMensagens} placeholder='Digite uma mensagem' placeholderTextColor='#bbbbbb' ></TextInput> 
-            <Image source={require('../../assets/icons/clip.png')} style={style.clip} />
-            <Image source={require('../../assets/icons/CameraZapChat.png')} style={style.camera} />
+            <TextInput onChangeText={onChangeText} value={text} style={style.caixaMensagens} placeholder='Digite uma mensagem' placeholderTextColor='#bbbbbb' autoCorrect={true} ></TextInput> 
+                <Image source={require('../../assets/icons/clip.png')} style={style.clip} />    
+                <Image source={require('../../assets/icons/CameraZapChat.png')} style={style.camera} />
+                <Image source={require('../../assets/icons/Emoji.png')} style={style.emoji} />
             <Image source={require('../../assets/icons/MicroZap.png')} style={style.micro} />
         </>
     )
--- screens/mensagens/style.js
+++ screens/mensagens/style.js
@@ -7,6 +7,129 @@ export const style = StyleSheet.create({
         height: 618,
         width: 360,
     },
+    mensagens: {
+        maxHeight: 560
+    },
+
+    mensagem1: {
+        position: 'absolute',
+        fontSize: 14,
+        marginLeft: 122,
+        marginTop: 30,
+        width: 220
+    },
+    data1: {
+        position: 'absolute',
+        fontSize: 12,
+        color: '#bbbbbb',
+        marginLeft: 300,
+        marginTop: 88
+    },
+
+    mensagem2: {
+        position: 'absolute',
+        fontSize: 14,
+        width: 210,
+        marginLeft: 17,
+        marginTop: 14
+    },
+    data2: {
+        position: 'absolute',
+        fontSize: 12,
+        color: '#bbbbbb',
+        marginLeft: 160,
+        marginTop: 20
+    },
+
+    mensagem3: {
+        position: 'absolute',
+        fontSize: 14,
+        width: 210,
+        marginLeft: 200,
+        marginTop: 6
+    },
+    data3: {
+        position: 'absolute',
+        fontSize: 12,
+        color: '#bbbbbb',
+        marginLeft: 305,
+        marginTop: 12
+    },
+
+    mensagem4: {
+        position: 'absolute',
+        fontSize: 14,
+        width: 210,
+        marginLeft: 20,
+        marginTop: 9
+    },
+    data4: {
+        position: 'absolute',
+        fontSize: 12,
+        color: '#bbbbbb',
+        marginLeft: 128,
+        marginTop: 16
+    },
+
+    mensagem5: {
+        position: 'absolute',
+        fontSize: 14,
+        width: 210,
+        marginLeft: 200,
+        marginTop: 9
+    },
+    data5: {
+        position: 'absolute',
+        fontSize: 12,
+        color: '#bbbbbb',
+        marginLeft: 303,
+        marginTop: 16
+    },
+
+    mensagem6: {
+        position: 'absolute',
+        fontSize: 14,
+        width: 260,
+        marginLeft: 20,
+        marginTop: 10
+    },
+    data6: {
+        position: 'absolute',
+        fontSize: 12,
+        color: '#bbbbbb',
+        marginLeft: 200,
+        marginTop: 16
+    },
+
+    mensagem7: {
+        position: 'absolute',
+        fontSize: 14,
+        width: 210,
+        marginLeft: 135,
+        marginTop: 9
+    },
+    data7: {
+        position: 'absolute',
+        fontSize: 12,
+        color: '#bbbbbb',
+        marginLeft: 303,
+        marginTop: 16
+    },
+
+    mensagem8: {
+        position: 'absolute',
+        fontSize: 14,
+        width: 210,
+        marginLeft: 125,
+        marginTop: 9
+    },
+    data8: {
+        position: 'absolute',
+        fontSize: 12,
+        color: '#bbbbbb',
+        marginLeft: 303,
+        marginTop: 72
+    },
 
     envio1: {
         marginTop: 15,
@@ -37,7 +160,7 @@ export const style = StyleSheet.create({
         paddingLeft: 40,
         marginTop: 565,
         fontSize: 18,
-        marginLeft: 5
+        marginLeft: 5,
     },
     emoji: {
         width: 22,
